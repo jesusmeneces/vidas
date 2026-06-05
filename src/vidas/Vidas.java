3d548841419eaea11127e302c73dd7728c07709e
jesusmeneces <joel.meneces8129@utc.edu.ec>
2026-06-05 15:59:26 -0500

    segundo commit

--- src/vidas/Vidas.java
+++ src/vidas/Vidas.java
@@ -16,6 +16,7 @@ public class Vidas {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("holassssisas");
+        System.out.println("isaac el que no se queda al voly");
     }
     
 }
